c4bd5f66243284d027d349a4f6eb5c5da4eee88e
Neothawreww <98629525+Neothawreww@users.noreply.github.com>
2022-03-26 18:36:43 -0500

    Update proto-gen.sh

--- js/scripts/proto-gen.sh
+++ js/scripts/proto-gen.sh
@@ -9,14 +9,14 @@ PROTOC_GEN_TS_PROTO_PATH="./node_modules/.bin/protoc-gen-ts_proto"
 
 mkdir -p "$OUT_DIR"
 
-echo "Processing terrad proto files ..."
-TERRAD_DIR="../terrad/proto"
-TERRAD_THIRD_PARTY_DIR="../terrad/third_party/proto"
+echo "Processing iqd proto files ..."
+IQD_DIR="../iqd/proto"
+IQD_THIRD_PARTY_DIR="../iqd/third_party/proto"
 
 protoc \
   --plugin="protoc-gen-ts_proto=${PROTOC_GEN_TS_PROTO_PATH}" \
   --ts_proto_out="${OUT_DIR}" \
   --ts_proto_opt="esModuleInterop=true,forceLong=long,useOptionals=true,outputClientImpl=grpc-web" \
-  --proto_path="$TERRAD_DIR" \
-  --proto_path="$TERRAD_THIRD_PARTY_DIR" \
-  $(find ${TERRAD_DIR} ${TERRAD_THIRD_PARTY_DIR} -path -prune -o -name '*.proto' -print0 | xargs -0)
+  --proto_path="$IQD_DIR" \
+  --proto_path="$IQD_THIRD_PARTY_DIR" \
+  $(find ${IQD_DIR} ${IQD_THIRD_PARTY_DIR} -path -prune -o -name '*.proto' -print0 | xargs -0)
